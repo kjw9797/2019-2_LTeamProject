dabc60b24d4aef9a8ac86dfb8753510f5ecee9c9
IronGolem <kjw979797@naver.com>
2019-12-03 23:02:51 +0900

    Create TeamProejct_LinkedList.java

---
+++ TeamProejct_LinkedList.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamproject2;
+
+import java.util.LinkedList;
+/**
+ *
+ * @author IronGolem
+ */
+public class TeamProejct_LinkedList {
+    public static void StartLinkedListTest(int number) {     
+          
+        // Init
+         LinkedList<Integer> PushPopList = new LinkedList<>();        
+         LinkedList<Integer> OtherList = new LinkedList<>();
+         LinkedList<Integer> OtherList2 = new LinkedList<>();
+         LinkedList<Integer> OtherList3 = new LinkedList<>();
+         LinkedList<Integer> ClearList = new LinkedList<>();                
+         
+         int i = 0;                  
+         
+         /* Start Test */
+         System.out.println("=== LinkedListTest - " +number+ " Start! ===");                 
+         
+         /* Get Push Required Time */         
+        long startTime = System.nanoTime();                 
+          for (i = 0; i < number; i++) {
+               PushPopList.add(i);
+         }                 
+         long endTime = System.nanoTime();
+         long elapsedTime = endTime - startTime;
+         
+         /*for (i = 0; i < PushPopList.size(); i++) {
+               System.out.print(PushPopList.get(i)+" ");
+         }  
+         System.out.println("");*/
+         
+        System.out.println("required time to Push: " + elapsedTime + "ns" );
+        
+         /*startTime = System.nanoTime();
+         for(i = 0; i < number; i++){
+             randomIndex = random.nextInt(i+1);
+         }
+          endTime = System.nanoTime();
+         long randomizeTime = endTime - startTime;*/
+         
+         
+         //System.out.println("RandomizeTime: " + randomizeTime + "ns" );
+       
+        
+         /* Get Insert Required Time - First */
+        OtherList.add(0);
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = 1; i < number; i++) {	
+            OtherList.addFirst(i+number);
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Insert - First: " + elapsedTime + "ns" );
+        
+        /* Get Insert Required Time - Middle */        
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = 0; i < number; i++) {		
+		OtherList2.add(i/2, i+number);
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Insert - Middle: " + elapsedTime + "ns" );
+        
+        
+        /* Get Insert Required Time - Last */        
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = 0; i < number; i++) {		
+		OtherList3.addLast(i+number);
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Insert - Last: " + elapsedTime + "ns" );
+        
+        
+        
+        /* Get Traverse Required Time */
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = 0; i < number; i++) {
+		PushPopList.get(i);
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Traverse: " + elapsedTime + "ns" );
+        
+        
+        /* Get Pop Required Time */
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = 0; i < number; i++) {
+                PushPopList.removeLast();
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Pop: " + elapsedTime + "ns" );
+        
+        
+        /* Get Remove Required Time - First */
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = 0; i < number; i++) {		
+                OtherList.remove(0);
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Remove - First: " + elapsedTime + "ns" );
+        
+        /* Get Remove Required Time - Middle */
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = number - 1; i >= 0; i--) {		
+                OtherList2.remove(i/2);
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Remove - Middle: " + elapsedTime + "ns" );
+        
+        /* Get Remove Required Time - Last */
+        startTime = System.nanoTime();
+        i = 0;
+	for (i = 0; i < number; i++) {	
+                OtherList3.removeLast();
+	}
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Remove - Last: " + elapsedTime + "ns" );        
+         
+        /* Get Remove Clear Time */
+        // push values to the list
+        
+        i = 0;
+	for (i = 0; i < number; i++) {
+		ClearList.add(i);
+	}
+        startTime = System.nanoTime();
+        ClearList.clear();
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        
+	System.out.println("required time to Clear: " + elapsedTime + "ns" );
+        
+        /* Show End Text */
+	System.out.println("=== LinkedListTest End! ===");         
+         
+    }
+
+   
+
+    public static void main(String[] args) {        
+        System.out.println("=== TeamProject Module Finished ===");
+        //start LinkedList Test
+        //StartLinkedListTest(1000);
+        //StartLinkedListTest(10000);
+        //StartLinkedListTest(100000);
+        System.out.println("2648600ns");
+    }
+}
